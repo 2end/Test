9434913e90f374f458f0b0d310f387e009b6aca3
Roman Myshchyshyn <roman.myshchyshyn@solarwinds.com>
2018-12-04 11:12:15 +0200

    conflict1

--- TestGit/Program.cs
+++ TestGit/Program.cs
@@ -11,7 +11,7 @@ namespace TestGit
         static void Main(string[] args)
         {
             Console.WriteLine("MASTER");
-            Console.WriteLine("CONFLICT");
+            Console.WriteLine("CONF1LICT");
         }
     }
 }
